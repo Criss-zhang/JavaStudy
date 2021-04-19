92899c1084c486c9665638224e6f8e161a1cbbc4
Criss-zhang <814329017@qq.com>
2021-04-19 14:01:10 +0800

    polyphism study

---
+++ src/Apple.java
@@ -0,0 +1,22 @@
+public class Apple extends Fruit{
+    @Override
+    public void info() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        Fruit apple = new Apple();
+        apple.num = 12;
+        apple.info();
+    }
+
+
+}
+class Fruit{
+
+    protected int num;
+
+    public void info(){
+        System.out.println("水果数量："+num);
+    }
+}
---
+++ src/ObjectTest.java
@@ -0,0 +1,14 @@
+public class ObjectTest {
+
+}
+class ooo{
+    private  String aa;
+    private  String bb;
+    public ooo(String aa,String bb){
+        this.aa = aa;
+        this.bb = bb;
+    }
+    public ooo(){
+        this(null,null);
+    }
+}
